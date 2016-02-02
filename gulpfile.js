7589064fc713843ebcf1a77edb65d6a794133e9f
lemoi <fy5036013@163.com>
2016-02-02 22:21:37 +0800

    add .map

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,22 @@ const babel=require('gulp-babel');
 const csso=require("gulp-csso");
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
- 
+const sourcemaps=require('gulp-sourcemaps');
 gulp.task('compressJS', function() {
   return gulp.src('src/smi_js/*.js')
  	.pipe(babel({presets:['es2015']}))
+ 	.pipe(sourcemaps.init())
     .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
 });
 gulp.task('compressCss', function() {
   return gulp.src("src/smi_css/*.css")
     .pipe(concat('smi.css'))
+    .pipe(sourcemaps.init())
     .pipe(csso())
-    .pipe(gulp.dest('dist'));
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'))
+   
 });
 gulp.task('default',['compressJS','compressCss']);
